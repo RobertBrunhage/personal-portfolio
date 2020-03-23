f43d83d5d10b4d5f31c4c89be69e93466ce03393
Robert Brunhage <robertbrunhage@gmail.com>
2020-03-23 18:07:14 +0100

    Add correct layout to Hero
    This also added style to call to action

--- src/components/shared/Cta/Cta.tsx
+++ src/components/shared/Cta/Cta.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './Cta.module.scss';
 
 const Cta: React.FC = () => (
-  <div className={styles.Cta} data-testid="Cta">
-    Cta Component
+  <div className={styles.cta} data-testid="Cta">
+    <a href="#"><span>MY WORK</span></a>
   </div>
 );
 
