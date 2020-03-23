e45801c3318d8e1a61c3408ffb6f1cab21cb821c
Robert Brunhage <robertbrunhage@gmail.com>
2020-03-23 12:18:46 +0100

    Add fontAwesome and basic mobile navbar
    The navbar will let you do a basic toggle.

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import styles from './NavigationBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const NavigationBar: React.FC = () => (
-  <div className={styles.NavigationBar} data-testid="NavigationBar">
-    NavigationBar Component
-  </div>
-);
+class NavigationBar extends React.Component {
+  constructor(props: never) {
+    super(props);
+    this.toggleMobileNavbar = this.toggleMobileNavbar.bind(this);
+  }
+
+  private mainNavRef = createRef<HTMLUListElement>()
+
+  toggleMobileNavbar() {
+    const navNode = this.mainNavRef.current;
+
+    if (navNode != null) {
+      navNode.classList.toggle(styles.active);
+    }
+  }
+
+  render() {
+    return (
+      <nav className={styles.navbar} data-testid="NavigationBar">
+        <a href="#" className={styles.logo}>logo</a>
+        <FontAwesomeIcon className={styles.navbarToggle} onClick={this.toggleMobileNavbar} icon={faBars} />
+        <ul className={styles.mainNav} ref={this.mainNavRef}>
+          <li>
+            <a href="#" className={styles.navLinks}>Home</a>
+          </li>
+          <li>
+            <a href="#" className={styles.navLinks}>Products</a>
+          </li>
+          <li>
+            <a href="#" className={styles.navLinks}>About Us</a>
+          </li>
+          <li>
+            <a href="#" className={styles.navLinks}>Contact Us</a>
+          </li>
+          <li>
+            <a href="#" className={styles.navLinks}>Blog</a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
 
 export default NavigationBar;
