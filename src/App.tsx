74fb41a48daaac5bbadcaa309c42257f2d7bd731
Robert Brunhage <robertbrunhage@gmail.com>
2020-03-23 16:27:36 +0100

    Add correct style to h1 in Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Hero from './components/Hero/Hero';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <Hero />
     </div>
   );
