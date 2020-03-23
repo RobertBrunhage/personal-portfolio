d3dd2c9711f2f77cfc62fa89a114e61fe9ec46d1
Robert Brunhage <robertbrunhage@gmail.com>
2020-03-23 16:27:36 +0100

    Add correct style to h1 in Hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './Hero.module.scss';
 import Cta from '../shared/Cta/Cta';
-import NavigationBar from '../NavigationBar/NavigationBar';
 
 const Hero: React.FC = () => (
   <div className={styles.Hero} data-testid="Hero">
-    <NavigationBar />
     <h1 className={styles.navbarPaddingTop}>Hi, I'm Robert. <br /> A software Engineer.</h1>
     <Cta />
   </div>
