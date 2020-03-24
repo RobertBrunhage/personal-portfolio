b1d58562da2900c8931c926acccef93416d5ad3c
Robert Brunhage <robertbrunhage@gmail.com>
2020-03-24 09:41:12 +0100

    Add additional tests for the hero section

--- src/components/NavigationBar/NavigationBar.test.tsx
+++ src/components/NavigationBar/NavigationBar.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, waitForElement } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import NavigationBar from './NavigationBar';
 
@@ -7,9 +7,36 @@ describe('<NavigationBar />', () => {
   afterEach(cleanup);
 
   test('it should mount', () => {
+    // ARRANGE
     const { getByTestId } = render(<NavigationBar />);
     const navigationBar = getByTestId('NavigationBar');
 
+    // ACT
+    //ASSERT
     expect(navigationBar).toBeInTheDocument();
+  });
+
+  test('given burger menu when page renders then display burger menu', () => {
+    // ARRANGE
+    const { getByTestId } = render(<NavigationBar />);
+    const burgerButton = getByTestId('BurgerButton');
+
+    // ACT
+    //ASSERT
+    expect(burgerButton).toBeInTheDocument();
+  });
+
+  test('given menu choices when page renders then display menu choices', async () => {
+    // ARRANGE
+    const { findByText } = render(<NavigationBar />);
+    const aboutMeText = await waitForElement(() => findByText('about me'.toUpperCase()));
+    const myWorkText = await waitForElement(() => findByText('my work'.toUpperCase()));
+    const contactText = await waitForElement(() => findByText('contact'.toUpperCase()));
+
+    // ACT
+    //ASSERT
+    expect(aboutMeText).toBeInTheDocument();
+    expect(myWorkText).toBeInTheDocument();
+    expect(contactText).toBeInTheDocument();
   });
 });
