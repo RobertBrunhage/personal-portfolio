367faf122f9d5e796e57d40ef00f7788457b41e9
Robert Brunhage <robertbrunhage@gmail.com>
2020-03-25 10:54:05 +0100

    Change title text in hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import Cta from '../shared/Cta/Cta';
 
 const Hero: React.FC = () => (
   <div className={[styles.Hero, styles.navbarPaddingTop].join(" ")} data-testid="Hero">
-    <h1 className={styles.header}>Hi, I'm Robert. <br /> A software Engineer.</h1>
+    <h1 className={styles.header}>Hello, I'm Robert Brunhage. <br /> I'm a software engineer.</h1>
     <div className={styles.cta}><Cta /></div>
   </div>
 );
