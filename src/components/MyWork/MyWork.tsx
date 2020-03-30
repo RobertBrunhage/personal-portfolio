573b489a32e6dffa8059d4a3a969568ff772cbf8
Robert Brunhage <robertbrunhage@gmail.com>
2020-03-30 06:58:55 +0200

    Add Project cards that is not clickable
    Later on these cards shall be clickable
    on the "Learn more" button.

--- src/components/MyWork/MyWork.tsx
+++ src/components/MyWork/MyWork.tsx
@@ -3,6 +3,25 @@ import styles from './MyWork.module.scss';
 
 import styleHelper from '../../Helper.module.scss';
 import Header from '../shared/Header/Header';
+import Project from '../Project/Project';
+
+import background from '../../assets/hero-background.webp'
+import { ProjectDetails } from '../../interfaces/ProjectDetails';
+
+const projects: ProjectDetails[] = [
+  {
+    title: "Testing Project",
+    background: background
+  },
+  {
+    title: "Testing Project",
+    background: background
+  },
+  {
+    title: "Testing Project",
+    background: background
+  },
+]
 
 const MyWork: React.FC = () => (
   <section className={styles.MyWork} data-testid="MyWork">
@@ -11,7 +30,9 @@ const MyWork: React.FC = () => (
         <Header title="MY WORK" />
       </div>
       <div className={styles.Projects}>
-      // Map through project list
+        {projects.map((project, index) => {
+          return <Project title={project.title} background={project.background} key={index} />
+        })}
       </div>
     </div>
 
---
+++ src/interfaces/ProjectDetails.ts
@@ -0,0 +1,4 @@
+export interface ProjectDetails {
+    title: string,
+    background: string,
+}
