4df5d56e95892b842324feea64c8d7d009059a26
Robert Brunhage <robertbrunhage@gmail.com>
2020-04-06 09:47:16 +0200

    Add contact form
    This will incorporate netflify to handle
    email sending.

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styles from './ContactMe.module.scss';
+import Header from '../shared/Header/Header';
+import ContactForm from './ContactForm/ContactForm';
+import styleHelper from '../../Helper.module.scss';
 
 const ContactMe: React.FC = () => (
-  <section className={styles.ContactMe} data-testid="ContactMe">
-    ContactMe Component
+  <section className={`${styleHelper.MaxWidthContainer} ${styles.ContactMe}`} data-testid="ContactMe">
+    <div className={styles.Header}>
+      <Header title="LET'S GET IN TOUCH" />
+    </div>
+    <div className={styles.ContactForm}>
+      <ContactForm />
+    </div>
   </section>
 );
 
