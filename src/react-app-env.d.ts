baa8289906e0f033c313e9f109663444da7a4896
Robert Brunhage <robertbrunhage@gmail.com>
2020-04-14 15:49:36 +0200

    Add enviroment variable for Kontent key
    This also involves overriding the type
    declarations for the process.env
    to get IntelliSense for all the
    added ones.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly NODE_ENV: 'development' | 'production' | 'test'
+        readonly PUBLIC_URL: string
+        readonly REACT_APP_PUBLIC_KONTENT_API: string
+    }
+}
