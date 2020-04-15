20e0e6b581d44422adfb2cf67af0596e1b5bba06
Robert Brunhage <robertbrunhage@gmail.com>
2020-04-15 09:36:31 +0200

    Add Kontent for Projects
    This involves creating a service
    and providing that with react context.
    Then display it in the MyWork section.

---
+++ src/services/Kontent/ProjectKontentService.ts
@@ -0,0 +1,31 @@
+import { KontentService } from "./KontentService";
+import { DeliveryClient, ContentItem } from "@kentico/kontent-delivery";
+
+import { Observable } from "rxjs";
+import { map, distinct, distinctUntilChanged } from 'rxjs/operators';
+import { Project } from "../../models/Project";
+
+export class ProjectKontentService implements KontentService<Project> {
+    private deliveryClient = new DeliveryClient({
+        projectId: process.env.REACT_APP_PUBLIC_KONTENT_API,
+    });
+
+    listenAll: Observable<Project[]> = this.deliveryClient.items<ContentItem>()
+        .type('project')
+        .toObservable()
+        .pipe(
+            map((response) => response.items.map((contentItem) => {
+                return new Project(
+                    contentItem.title?.value,
+                    contentItem.image?.value[0]?.url,
+                    contentItem.description?.value,
+                    contentItem.weblink?.value,
+                    contentItem.weblinkname?.value,
+                    contentItem.weblink2?.value,
+                    contentItem.weblink2name?.value,
+                    contentItem.githublink?.value,
+                    contentItem.date?.value,
+                );
+            })),
+        )
+}
